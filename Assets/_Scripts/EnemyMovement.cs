40eea8988ee989517efefd3795c92d2083891d1b
Erik Rensink <erensink@gmail.com>
2019-12-11 12:10:45 +0100

    Updated to Unity 2019.3.0.f1

---
+++ Assets/_Scripts/EnemyMovement.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] ParticleSystem EndParticle;
+    [SerializeField] float moveSpeed = 5.0f;
+    [SerializeField] float lookSpeed = 5.0f;
+    [SerializeField] List<Waypoint> path;
+
+    private Transform toGoTo;
+
+    // Use this for initialization
+    void Start()
+    {
+        //pathFinder is my waypoint calculator / breadths first search calculator
+        Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
+        //path is the list of waypoints in pathfinder from start to finish and is retrieved with a function to avoid unwanted editing 
+        path = pathfinder.GetPath();
+        //starts the followpath routine and passes it the path list from pathfinder
+        StartCoroutine(FollowPath(path));
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    //runs every frame for smooth movement (could put in Update but if I needed to expand script/ add more functions to the update I would have to do this anyways)
+    void Move()
+    {
+        float MovementSpeed = moveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, toGoTo.position, MovementSpeed);
+
+        Vector3 relativePos = toGoTo.position - transform.position;
+        Quaternion toRotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, lookSpeed * Time.deltaTime);
+    }
+
+    IEnumerator FollowPath(List<Waypoint> wppath)
+    {
+        foreach (Waypoint waypoint in wppath)
+        {
+            toGoTo = waypoint.transform;
+            yield return new WaitUntil(() => isAtPosition(waypoint) == true);
+        }
+        ReachedEnd();
+    }
+
+    //returns true when enemy has reached the position of the next waypoint
+    bool isAtPosition(Waypoint wp)
+    {
+        if (this.transform.position == wp.transform.position)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //things to do once object has reached end tile
+    void ReachedEnd()
+    {
+        var deathParticle = Instantiate(EndParticle, this.transform.position, Quaternion.identity, this.transform.parent.transform);
+        deathParticle.Play();
+        Destroy(deathParticle.gameObject, deathParticle.main.duration);
+        Destroy(this.gameObject);
+    }
+
+    //just a setter for scriptable objects
+    public void setSpeed(float speed)
+    {
+        moveSpeed = speed;
+    }
+
+}
---
+++ Assets/_Scripts/Pathfinder.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    public float chanceToSpawn = 0.1f;
+    [SerializeField] Waypoint startWaypoint = null, endWaypoint = null;
+
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+    bool isRunning = true;
+    Waypoint searchCenter;
+    List<Waypoint> path = new List<Waypoint>();
+
+    Vector2Int[] directions = {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
+    public List<Waypoint> GetPath()
+    {
+        if (path.Count == 0)
+        {
+            CalculatePath();
+        }
+        return path;
+    }
+
+    private void CalculatePath()
+    {
+        LoadBlocks();
+        BreadthFirstSearch();
+        CreatePath();
+        ColorWorld();
+    }
+
+    private void ColorWorld()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            if (!waypoint.isPlaceable && path.Contains(waypoint))
+            {
+                GameObject tile = waypoint.transform.Find("Block_Friendly/tile_waypoint").gameObject;
+                if (tile != null)
+                {
+                    tile.SetActive(true);
+                }
+            }
+            else if (!waypoint.isPlaceable && !path.Contains(waypoint))
+            {
+                GameObject tile = waypoint.transform.Find("Block_Friendly/tile_rock").gameObject;
+                if (tile != null)
+                {
+                    tile.SetActive(true);
+                }
+            }
+            else
+            {
+                GameObject tile = waypoint.transform.Find("Block_Friendly/tile_grass").gameObject;
+                if (tile != null)
+                {
+                    tile.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void CreatePath()
+    {
+        SetAsPath(endWaypoint);
+
+        Waypoint previous = endWaypoint.exploredFrom;
+        while (previous != startWaypoint)
+        {
+            SetAsPath(previous);
+            previous = previous.exploredFrom;
+        }
+
+        SetAsPath(startWaypoint);
+        path.Reverse();
+    }
+
+    private void SetAsPath(Waypoint waypoint)
+    {
+        path.Add(waypoint);
+        waypoint.isPlaceable = false;
+    }
+
+    private void BreadthFirstSearch()
+    {
+        queue.Enqueue(startWaypoint);
+
+        while (queue.Count > 0 && isRunning)
+        {
+            searchCenter = queue.Dequeue();
+            HaltIfEndFound();
+            ExploreNeighbours();
+            searchCenter.isExplored = true;
+        }
+    }
+
+    private void HaltIfEndFound()
+    {
+        if (searchCenter == endWaypoint)
+        {
+            isRunning = false;
+        }
+    }
+
+    private void ExploreNeighbours()
+    {
+        if (!isRunning) { return; }
+
+        foreach (Vector2Int direction in directions)
+        {
+            //if (Random.Range(0.0f, 1.0f) > chanceToSpawn)
+            //{
+            //    break;
+            //}
+            Vector2Int neighbourCoordinates = searchCenter.GetGridPos() + direction;
+            if (grid.ContainsKey(neighbourCoordinates))
+            {
+                QueueNewNeighbours(neighbourCoordinates);
+            }
+        }
+    }
+
+    private void QueueNewNeighbours(Vector2Int neighbourCoordinates)
+    {
+        Waypoint neighbour = grid[neighbourCoordinates];
+        if (neighbour.isExplored || queue.Contains(neighbour))
+        {
+            // do nothing
+        }
+        else
+        {
+            queue.Enqueue(neighbour);
+            neighbour.exploredFrom = searchCenter;
+        }
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            if (grid.ContainsKey(gridPos))
+            {
+                Debug.LogWarning("Skipping overlapping block " + waypoint);
+            }
+            else
+            {
+                grid.Add(gridPos, waypoint);
+            }
+        }
+    }
+}
